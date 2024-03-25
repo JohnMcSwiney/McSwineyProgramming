78619771016a250e3edaba059edd21afcae54dc1
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2024-03-25 11:06:45 -0600

    Uncommitted work from Mac Air

--- src/components/ProjectBox/ProjectCont.js
+++ src/components/ProjectBox/ProjectCont.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './ProjectStyles.css';
 
 function ProjectCont({children}) {
   return (
     <div className='proj-parent-container'>
+      <div className="sub-title mar-l-a ">
+        <h3 className="outfit-700">Projects:</h3>
+        <svg className="sub-title-accent">
+          <rect width="60" height="5" />
+        </svg>
+      </div>
         {children}
     </div>
   )
