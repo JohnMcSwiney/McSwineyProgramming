c33a20d0de5571eb68f16696f599d0b9e6edd9ce
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2024-03-29 17:56:19 -0600

    Switching to Windows

--- src/components/ProjectBox/ProjectTrove.js
+++ src/components/ProjectBox/ProjectTrove.js
@@ -4,6 +4,9 @@ import './ProjectStyles.css'
 
 import projectJson from './Projects.json'
 
+import { FaReact } from "react-icons/fa";
+
+
 function ProjectTrove({index}) {
     const [evenIndex, setEvenIndex] = useState(false);
   function isEven(n){
@@ -18,7 +21,9 @@ function ProjectTrove({index}) {
   return (
     <div className={`project-component ${evenIndex ? "project-dark-bg" : "project-light-bg"}`}>
       <h2>{projectJson.projects[0].name}</h2>
+      <h3>{projectJson.projects[0].date}</h3>
       <p>{projectJson.projects[0].desc}</p>
+      <div><div className="icon-cont" ><FaReact className="icon"/></div></div>
       {projectJson.projects[0].URL != ""  &&
        <a
        href={projectJson.projects[0].URL} 
